3b6d4de4450c873a7ae680ee53f635101765235b
roman <roman.yakhimovich@gmail.com>
2026-01-04 11:27:27 +0200

    Add `askYesNo` function and main enhancements for password generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,3 +24,25 @@ fun askForInt(message: String, min: Int, max: Int): Int {
         println("Please enter an integer between $min and $max")
     }
 }
+fun askYesNo(question: String): Boolean {
+    while (true) {
+        print("$question (y/n) ")
+        val input = readlnOrNull()?.trim()
+        when (input) {
+            "y", "yes", "tak" -> return true
+            "n", "no", "nie" -> return false
+            else -> println("Please answer with 'y' for yes or 'n' for no! ")
+        }
+    }
+}
+fun main() {
+    println("Password generator from Roman!")
+    val length = askForInt("Enter the password length (recommended 8-20):", min = 4, max = 100)
+    val useUppercase = askYesNo("Do you want to use uppercase letters (A-Z)? ")
+    val useDigits = askYesNo("Do you want to use digits (0-9)? ")
+    val useChars = askYesNo("Do you want to use special characters (!@#$)? ")
+    val password = generatePassword(length, useUppercase, useDigits, useChars)
+    println("----------------------------------")
+    println("Your password is: $password")
+    println("----------------------------------")
+}
